1476671439f61cb723dbdb16c7f736d63069e950
Igor Deon <igordeon709@gmail.com>
2021-01-06 01:34:15 -0300

    Ready to prod / attempt 2

--- backend/src/main/java/com/devsuperiorevento/dsdeliver/DsdeliverApplication.java
+++ backend/src/main/java/com/devsuperiorevento/dsdeliver/DsdeliverApplication.java
@@ -2,8 +2,9 @@ package com.devsuperiorevento.dsdeliver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DsdeliverApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/devsuperiorevento/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperiorevento/dsdeliver/repositories/OrderRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.devsuperiorevento.dsdeliver.entities.Order;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 	
 	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products"
