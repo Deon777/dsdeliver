35317886976de2b19217fbb0f168111a5f49c5fd
Igor Deon <igordeon709@gmail.com>
2021-01-06 18:50:45 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home/index'
 
 function App() {
     return (
       <div>
         <Navbar/>
+        <Home/>
       </div>
     );
 }
---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,35 @@
+import './styles.css'
+import { ReactComponent as HomeImage } from './main.svg'
+import { defaultMaxListeners } from 'stream'
+
+function Home () {
+    return (
+
+        <div className="home-container">
+            <div className="home-content">
+                <div className="home-actions">
+
+                    <h1 className="home-title">
+                        Faça seu pedido <br/>
+                        que entregamos  <br/>
+                        pra você!!!
+                    </h1>
+                    <h3 className="home-subtitle">
+                        Escolha o seu pedido e em poucos minutos <br/>
+                        levaremoss na sua porta
+                    </h3>
+                    <a href="orders" className="home-bttn-order">
+                        FAZER PEDIDO
+                    </a>
+
+                </div>
+                <div className="home-image">
+                    <HomeImage/>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Home;
