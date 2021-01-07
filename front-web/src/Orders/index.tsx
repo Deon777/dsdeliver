bfc53e81ae863750eed0365ae8c115cc40de9a52
Igor Deon <igordeon709@gmail.com>
2021-01-07 03:56:34 -0300

    Add products from back end

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,24 @@
 import "./styles.css"
 import StepsHeader from "./StepsHeader"
+import ProducsList from "./ProductsList"
+import { useEffect, useState } from "react";
+import { Product } from "./types";
+import { fetchProducts } from "../api";
 
 function Orders() {
+
+    const [products, SetProducts] = useState<Product[]>([]);
+    
+    useEffect(() => {
+        fetchProducts()
+        .then(response => SetProducts(response.data))
+        .catch(error => console.log(error));
+    }, []);
+
     return (
-        <div>
+        <div className="orders-container">
             <StepsHeader/>
+            <ProducsList products={products} />
         </div>
     )
 }
